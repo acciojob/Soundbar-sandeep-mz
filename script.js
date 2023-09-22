f20e34f4e982417f8a7b6fad3bdefce7398d90d6
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-09-23 02:18:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+  const video = document.getElementsByTagName("video")[0];
+
+        function playVideo() {
+          // plays the video
+          video.play();
+        }
+
+        function pauseVideo() {
+          video.pause();
+        }
+
+        function backward() {
+          video.currentTime -= 5;
+        }
+
+        function forward() {
+          video.currentTime += 5;
+        }
